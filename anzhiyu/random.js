e218ca80a450583bc68b252835c9a4b98e95c777
erheqi <erheqi@gmail.com>
2025-10-02 17:42:22 +0800

    Site updated: 2025-10-02 17:42:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/02/hello-world/"];function toRandomPost(){
+var posts=["2025/10/02/Hexo-博客搭建全流程指南/","2025/10/02/Hello-World-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
